44611754ee12e12c291dc5398c28bcf2921a47b5
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-26 16:04:48 -0300

    Termina la construción de clases e interfaces de command

--- clase_03/src/ar/com/educacionit/command/editor_texto/CommandManager.java
+++ clase_03/src/ar/com/educacionit/command/editor_texto/CommandManager.java
@@ -1,5 +1,22 @@
 package ar.com.educacionit.command.editor_texto;
 
+import java.util.Stack;
+
+import ar.com.educacionit.command.editor_texto.interfaces.Command;
+
 public class CommandManager {
+	
+	private Stack<Command> history = new Stack<>();
+	
+	public void ejecutarComando(Command comando) {
+		comando.ejecutar();
+		history.push(comando);
+	}
+	
+	public void deshacerUltimo() {
+		if ( !history.isEmpty() ) {
+			history.pop().volverAtras();
+		}
+	}
 
 }
