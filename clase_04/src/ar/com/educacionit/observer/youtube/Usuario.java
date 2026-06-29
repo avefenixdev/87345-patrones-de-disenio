840e3ff13ec3fc5b9dcd26ac0ad671eb145a1ee8
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-29 16:01:04 -0300

    Implementadas las clases del ejemplo

--- clase_04/src/ar/com/educacionit/observer/youtube/Usuario.java
+++ clase_04/src/ar/com/educacionit/observer/youtube/Usuario.java
@@ -1,5 +1,20 @@
 package ar.com.educacionit.observer.youtube;
 
-public class Usuario {
+import ar.com.educacionit.observer.interfaces.Suscriptor;
+// Suscriptor concreto -> Observador concreto, tiene que respetar el contrato Suscriptor.
+
+public class Usuario implements Suscriptor {
+	
+	private String nombre;
+	
+	public Usuario(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public void notificar(String video) {
+		System.out.println(this.nombre + " recibió la notificación " + video);
+		
+	}
 
 }
